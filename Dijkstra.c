f6def28d7c1df81c4af871ce093a2bc24f4450e6
Wiffer <wifferlin0505@gmail.com>
2017-11-25 11:41:16 +0800

    Added the input function

--- Dijkstra.c
+++ Dijkstra.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     int count1 = 0 , count2 = 0;
     int Node1 = 0 , Node2 = 0; 
     int ignore;
-
+    int Source , Destination; 
     //open the topology file
     FILE *file;
     file = fopen("cnstln36.txt","r");
@@ -17,23 +17,16 @@ int main(int argc, char *argv[])
     fscanf(file,"%d",&Num_of_Node);
     fscanf(file,"%d",&Num_of_EndNode);
     fscanf(file,"%d",&Num_of_Link);
-    printf("%d %d %d\n",Num_of_Node,Num_of_Link,Num_of_EndNode);
+    //printf("%d %d %d\n",Num_of_Node,Num_of_Link,Num_of_EndNode);
 
 
-    //Initial the Link Matrix of Node(ALL Link are Infinity First)
+    //Initial the Link Matrix of Node(ALL Link are 0 First)
     int LinkMatrix[Num_of_Node][Num_of_Node];
     for(count1 = 0; count1 < Num_of_Node; count1++)
     {
         for(count2 = 0;count2 < Num_of_Node;count2++)
         {
-            if(count1 == count2)
-            {
-                LinkMatrix[count1][count2]=0;
-            }
-            else
-            {
-                LinkMatrix[count1][count2]=Infinity;
-            }
+            LinkMatrix[count1][count2]=0;
         }
     }
 
@@ -57,4 +50,27 @@ int main(int argc, char *argv[])
         printf("\n");
     }  
 */
+    printf("Please key in the Source Node:");
+    scanf("%d",&Source);
+    printf("\nPlease key in the Destination Node:");
+    scanf("%d",&Destination);
+    Dijkstra(Source,Destination,Num_of_Node,LinkMatrix[Num_of_Node][Num_of_Node]);
+}
+
+void Dijkstra(int Source , int Destination,int Num_of_Node, int LinkMatrix[Num_of_Node][Num_of_Node])
+{
+    int Distance[Num_of_Node];
+    int count;
+    for(count = 0;count < Num_of_Node;count++)
+    {
+        if(LinkMatrix[Source][count] == 1)
+        {
+            if(count == Source)
+            {
+                Distance[count] = 0;
+            }
+            Distance[count] = 1;
+        }
+        Distance[count] = Infinity;
+    }
 }
